f138cab69968b00ceba7281b89bef2322f8ad238
Alisson Dias <alissondias1523@gmail.com>
2025-07-07 17:58:16 -0300

    att

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
   const form = document.querySelector("form");
 
   form.addEventListener("submit", function(e) {
-    const nome = form.name.value.trim();
-    const email = form.email.value.trim();
-    const msg = form.message.value.trim();
+    const nome = form.elements["nome"].value.trim();
+    const email = form.elements["email"].value.trim();
+    const mensagem = form.elements["mensagem"].value.trim();
 
-    if (!nome || !email || !msg) {
-      e.preventDefault();
-      alert("Por favor, preencha todos os campos.");
+    if(!nome || !email || !mensagem){
+      e.preventDefault();//Impede o envio
+      alert("Por favor,preencha todos os campos obrigatórios.");
     }
   });
+   
